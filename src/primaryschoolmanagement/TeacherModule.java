246624d1b9696fed99f220bf1b600be774ab0ccd
SteveHoang <phamvanhoang11072000@gmail.com>
2023-06-11 07:24:30 +0700

    Teacher Module - 1.2

---
+++ src/primaryschoolmanagement/TeacherModule.java
@@ -0,0 +1,11 @@
+package primaryschoolmanagement;
+
+import javax.swing.*;
+
+public class TeacherModule extends JFrame {
+    public static void main(String[] args) {
+        TeacherModule teacherModule = new TeacherModule();
+        teacherModule.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        teacherModule.setVisible(true);
+    }
+}
